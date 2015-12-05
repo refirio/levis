5bd1ad86c832f727227b2bf34147bcfffc69a6a6
refirio <refirio@gmail.com>
2015-12-06 01:21:57 +0900

    Ver 6.0.1

--- libs/cores/version.php
+++ libs/cores/version.php
@@ -6,5 +6,5 @@
 
 *********************************************************************/
 
-define('VERSION_NUMBER', '6.0.0');
-define('VERSION_UPDATE', '2015-11-29');
+define('VERSION_NUMBER', '6.0.1');
+define('VERSION_UPDATE', '2015-12-06');
