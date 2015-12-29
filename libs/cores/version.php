795a8a1a62c3fc752cae96c4ceda3eadb8680faf
refirio <refirio@gmail.com>
2015-12-29 21:02:57 +0900

    Ver 6.3.0

--- libs/cores/version.php
+++ libs/cores/version.php
@@ -6,5 +6,5 @@
 
 *********************************************************************/
 
-define('VERSION_NUMBER', '6.2.1');
-define('VERSION_UPDATE', '2015-12-27');
+define('VERSION_NUMBER', '6.3.0');
+define('VERSION_UPDATE', '2015-12-29');
