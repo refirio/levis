90173c95b7d7e401c34fab3118d38d60bf255c59
refirio <refirio@gmail.com>
2016-12-24 13:51:02 +0900

    Ver 8.2.0

--- libs/cores/version.php
+++ libs/cores/version.php
@@ -6,5 +6,5 @@
 
 *******************************************************************************/
 
-define('VERSION_NUMBER', '8.1.2');
-define('VERSION_UPDATE', '2016-12-18');
+define('VERSION_NUMBER', '8.2.0');
+define('VERSION_UPDATE', '2016-12-24');
