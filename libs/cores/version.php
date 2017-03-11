533338ebd6694c5ee622aea77857be44a3e7695e
refirio <refirio@gmail.com>
2017-03-11 17:26:43 +0900

    Ver 8.5.0

--- libs/cores/version.php
+++ libs/cores/version.php
@@ -6,5 +6,5 @@
 
 *******************************************************************************/
 
-define('VERSION_NUMBER', '8.4.2');
-define('VERSION_UPDATE', '2017-02-23');
+define('VERSION_NUMBER', '8.5.0');
+define('VERSION_UPDATE', '2017-03-11');
