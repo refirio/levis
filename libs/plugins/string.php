b747407fcc83ade4cd766050950639ab15c3345e
refirio <refirio@gmail.com>
2021-07-01 23:48:09 +0900

    #108 文字列自動改行のための関数を追加

--- libs/plugins/string.php
+++ libs/plugins/string.php
@@ -20,3 +20,21 @@ function string_autolink($string)
 
     return $string;
 }
+
+/**
+ * Get a word-wrapped string.
+ *
+ * @param string $string
+ * @param int    $width
+ * @param string $break
+ *
+ * @return string
+ */
+function string_wordwrap($string, $width = 256, $break = "\n")
+{
+    $pattern = "/(.{1,{$width}})(?:\\s|$)|(.{{$width}})/uS";
+    $replace = '$1$2' . $break;
+    $wrapped = preg_replace($pattern, $replace, $string);
+
+    return $wrapped;
+}
