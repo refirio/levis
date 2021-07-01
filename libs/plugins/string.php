4730e9573f8fc4578fcd115d22cc00ba3ae951fc
refirio <refirio@gmail.com>
2021-07-01 23:47:06 +0900

    #108 オートリンク処理のルールを調整

--- libs/plugins/string.php
+++ libs/plugins/string.php
@@ -15,8 +15,8 @@
  */
 function string_autolink($string)
 {
-    $string = preg_replace('/(^|[^\"\w\.\~\-\/\?\&\#\+\=\:\;\@\%\!])(https?\:\/\/[\w\.\~\-\/\?\&\#\+\=\:\;\@\%\!]+)/', '$1<a href="$2">$2</a>', $string);
-    $string = preg_replace('/(^|[^\"\w\.\~\-\/\?\&\#\+\=\:\;\@\%\!])([\w\.\+\-]+@[\w\.\+\-]+)/', '$1<a href="mailto:$2">$2</a>', $string);
+    $string = preg_replace('/(^|[^\w\~\-\/\&\#\+\=\@\%])(https?\:\/\/[\w\.\~\-\/\?\&\#\+\=\:\;\@\%\!]+)/', '$1<a href="$2">$2</a>', $string);
+    $string = preg_replace('/(^|[^\w\~\-\/\&\#\+\=\@\%])([\w\.\+\-]+@[\w\.\+\-]+)/', '$1<a href="mailto:$2">$2</a>', $string);
 
     return $string;
 }
